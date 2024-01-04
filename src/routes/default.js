d0b3a7daf9ead2b91d6dbbe814b4185903b34632
Illyakravchuk <ilakrawchyk18@gmail.com>
2024-01-04 07:14:42 +0200

    improved the default page

--- src/routes/default.js
+++ src/routes/default.js
@@ -1,5 +1,6 @@
 function defaultHandler(req, res, payload, body) {
-  const defaultPage = '<h1>Method Not Implemented</h1>'
+  const defaultPage =
+    '<h1>Welcome to the default page</h1><p>available routers:</p><ul><li>/information</li><li>/greetings</li><li>/weather</li><li>/books</li></ul>'
 
   res.writeHead(200, {
     'Content-Type': 'text/html'

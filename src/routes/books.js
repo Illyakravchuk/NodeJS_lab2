d13b5f3d31686fd88630aa1bf4d53c2fc824cf87
Illyakravchuk <ilakrawchyk18@gmail.com>
2024-01-04 04:21:11 +0200

    added the ability for routers to handle OPTIONS and POST requests

--- src/routes/books.js
+++ src/routes/books.js
@@ -11,4 +11,17 @@ function GET(req, res, payload, body) {
   res.json(books)
 }
 
-export { GET }
+function OPTIONS(req, res, payload, body) {
+  res.writeHead(204, {
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'GET, OPTIONS, POST',
+    'Access-Control-Allow-Headers': 'Content-Type'
+  })
+  res.end()
+}
+
+function POST(req, res, payload, body) {
+  res.json(payload)
+}
+
+export { GET, OPTIONS, POST }
